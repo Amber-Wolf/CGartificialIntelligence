a2148249bd528720bb22530db8cb032111f0cc4f
Amber W <wolfercm@miamioh.edu>
2014-11-09 16:50:06 -0500

    Made Changes to the Evaluation method.

--- src/clobber/EvalState.java
+++ src/clobber/EvalState.java
@@ -1,5 +1,79 @@
 package clobber;
+import game.Util;
+
+import java.awt.Point;
+import java.util.ArrayList;
 
 public class EvalState {
+	
+	public static double PossMoves(ClobberState cs){
+		int val = 0;
+		char board[][] = null;
+		Util.copy(cs.board, board);
+		ClobberState temp = (ClobberState) cs.clone();
+		ArrayList<Point> homeLivePieces = new ArrayList();
+		ArrayList<Point> awayLivePieces = new ArrayList();
+		ArrayList<PieceGroup> groups = new ArrayList();
+		int x;
+		int y;
+		for(x = 0; x<cs.ROWS; x++){
+			for(y = 0; y<cs.COLS; y++){
+				if(checkLive(cs,board,x,y)){
+					handleLivePiece(cs,board,x,y,groups,homeLivePieces,awayLivePieces);
+					PieceGroup pg = new PieceGroup(cs,board,x,y);
+				}
+			}
+		}
+		double value = 0;
+		for(PieceGroup pg : groups){
+			if(pg.getSide() == cs.awaySym){
+				value -= pg.eval(homeLivePieces);
+			}else{
+				value += pg.eval(awayLivePieces);
+			}
+		}
+		return value;
+	}
+	
+	private static void handleLivePiece(ClobberState cs, char[][] board, int x,int y, 
+			ArrayList<PieceGroup> groups, ArrayList<Point> homeLivePieces, ArrayList<Point> awayLivePieces){
+		Point p = new Point(x,y);
+		boolean containedInList = false;
+		for(PieceGroup pg : groups){
+			if(pg.checkContains(x, y)){
+				containedInList = true;
+			}
+		}
+		if(!containedInList){
+			PieceGroup pg = new PieceGroup(cs,board,x,y);
+			groups.add(pg);
+		}
+		if(board[x][y] == cs.homeSym){
+			homeLivePieces.add(p);
+		}else{
+			awayLivePieces.add(p);
+		}
+	}
+	
+	private static boolean checkLive(ClobberState cs, char[][] board, int x, int y){
+		if(x >= cs.ROWS || x < 0 || y >= cs.COLS || y < 0 || board[x][y] == cs.emptySym){
+			return false;
+		}
+		if(checkOpposite(cs, board, x + 1, y, board[x][y]) || 
+				checkOpposite(cs, board, x, y + 1, board[x][y]) ||
+				checkOpposite(cs, board, x - 1, y, board[x][y]) || 
+				checkOpposite(cs, board, x, y - 1, board[x][y]) ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	private static boolean checkOpposite(ClobberState cs, char[][] board, int x, int y, char lastState){
+		if(x >= cs.ROWS || x < 0 || y >= cs.COLS || y < 0 || board[x][y] == cs.emptySym || board[x][y] == lastState){
+			return false;
+		}
+		return true;
+	}
 
 }
--- src/clobber/PieceGroup.java
+++ src/clobber/PieceGroup.java
@@ -1,5 +1,95 @@
 package clobber;
 
-public class PieceGroup {
+import java.util.ArrayList;
+import java.awt.Point;
 
+public class PieceGroup {
+	
+	//static char homeSym;
+	//static char awaySym;
+	//static char emptySym;
+	protected ArrayList<Point> pieces;
+	protected char groupType;
+	protected ClobberState cs;
+	protected char[][] board;
+	//ArrayList<PieceGroup> otherGroups;
+	
+	public PieceGroup(ClobberState cs, char[][] board, int x, int y){
+		Point p = new Point(x,y);
+		groupType = board[x][y];
+		this.cs = cs;
+	}
+	
+	private boolean checkSame(int x, int y){
+		if(x >= cs.ROWS || x < 0 || y >= cs.COLS || y < 0 || board[x][y] == cs.emptySym || board[x][y] != groupType){
+			return false;
+		}
+		return true;
+	}
+	
+	private void recursiveAdd(int x,int y){
+		if(checkSame(x+1,y) && !checkContains(x+1,y)){
+			Point p = new Point(x+1,y);
+			pieces.add(p);
+			recursiveAdd(x+1,y);
+		}
+		if(checkSame(x-1,y) && !checkContains(x-1,y)){
+			Point p = new Point(x-1,y);
+			pieces.add(p);
+			recursiveAdd(x-1,y);
+		}
+		if(checkSame(x,y+1) && !checkContains(x,y+1)){
+			Point p = new Point(x,y+1);
+			pieces.add(p);
+			recursiveAdd(x,y+1);
+		}
+		if(checkSame(x,y-1) && !checkContains(x,y-1)){
+			Point p = new Point(x,y-1);
+			pieces.add(p);
+			recursiveAdd(x,y-1);
+		}
+	}
+	
+	public boolean checkContains(int x,int y){
+		return pieces.contains(new Point(x,y));
+	}
+	
+	public char getSide(){
+		return groupType;
+	}
+	
+	private boolean withinOne(Point p){
+		for(Point myP: pieces){
+			if(p.getX() == myP.getX()){
+				if(Math.abs(p.getY() - p.getY()) < 1.5f){
+					return true;
+				}
+			}
+			if(p.getY() == myP.getY()){
+				if(Math.abs(p.getX() - p.getX()) < 1.5f){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public double eval(ArrayList<Point> livePieces){
+		double value = pieces.size();
+		int counter = 0;
+		for(Point p: livePieces){
+			if(withinOne(p)){
+				counter++;
+			}
+		}
+		value = value/counter;
+		if(value < 1f){
+			value = 1f;
+		}
+		return value;
+	}
 }
+	
+	
+	
+	
